3836f032f4be7320fed82cd82e38a6e806cb69b9
David Amorim <davidfilipeamorim@gmail.com>
2023-12-26 22:14:23 +0000

    Add test program for GraphFromFile

---
+++ example4.c
@@ -0,0 +1,38 @@
+//
+// Algoritmos e Estruturas de Dados --- 2023/2024
+//
+// David Amorim - Dec 2023
+//
+// Graph EXAMPLE : Reading graph from file
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Graph.h"
+
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    fprintf(stderr, "ERROR: No input graph file is provided!\n");
+    exit(1);
+  }
+
+  char* filepath = argv[1];
+
+  FILE* fp = fopen(filepath, "r");
+
+  if (fp == NULL) {
+    perror("ERROR: Could not open the file");
+    exit(1);
+  }
+
+  Graph* g = GraphFromFile(fp);
+
+  fclose(fp);
+
+  GraphDisplay(g);
+
+  GraphDestroy(&g);
+
+  return 0;
+}
